3f10ed0429fd22cb2f473b1cbe7511a698cd524e
shenshuiBUPT <shenshui@bupt.edu.cn>
2021-11-05 21:12:05 +0800

    update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import time
 
 n_max_threads = 4
 loops = 2
+chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"
 
 
 def watchVideo(browser: webdriver.Chrome,
@@ -31,13 +32,13 @@ def watchVideo(browser: webdriver.Chrome,
         action.move_to_element(video_zone).perform()
         speed = browser.find_element_by_class_name('xt_video_player_common_value')
         action.move_to_element(speed).perform()
-        # time.sleep(0.1)
         speed2 = browser.find_element_by_xpath(
             '//*[@id="video-box"]/div/xt-wrap/xt-controls/xt-inner/xt-speedbutton/xt-speedlist/ul/li[1]')
         action.move_to_element(speed2).perform()
         time.sleep(3)
         speed2.click()
         time.sleep(3)
+        action.move_to_element(video_zone).perform()
         end_time = browser.find_element_by_xpath(
             '//*[@id="video-box"]/div/xt-wrap/xt-controls/xt-inner/xt-time/span[2]')
         begin_time = browser.find_element_by_xpath('//*[@id="video-box"]/div/xt-wrap/xt-controls/xt-inner/xt-time'
@@ -65,28 +66,32 @@ def watchVideo(browser: webdriver.Chrome,
 
 
 def main():
+    # config
     options = webdriver.ChromeOptions()
-    options.binary_location = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+    options.binary_location = chrome_path
 
     browser = webdriver.Chrome('./chromedriver.exe', options=options)
     browser.get('https://bupt.yuketang.cn/pro/lms/84hxA9Q7Bn5/7899241/studycontent')
     time.sleep(3)
+    # login. you have 20 seconds to scan the QR code with your phone to login.
+    # you can also fine-tune the code to add or decrease the time.
     browser.find_element_by_xpath('//*[@id="app"]/div[2]/div[2]/div[3]/div/div[1]/div/div/div[2]/button/span').click()
     time.sleep(20)
 
     browser.find_element_by_xpath('/html/body/div[4]/div[2]/div[2]/div[3]/div/ul/li[4]').click()
     time.sleep(10)
-    videos = browser.find_elements_by_class_name('unit-name-hover')[:123]
-    texts = [i.text for i in browser.find_elements_by_class_name('concrete-tr')][:123]
 
     fifo_receiver, fifo_sender = Pipe(False)
     token_receiver, token_sender = Pipe(False)
     for i in range(n_max_threads):
         fifo_sender.send('a line')
     token_sender.send('token line')
-    threads = []
-    print('videos:', videos, sep='\n')
+
     for loop in range(loops):
+        threads = []
+        videos = browser.find_elements_by_class_name('unit-name-hover')[:123]
+        texts = [i.text for i in browser.find_elements_by_class_name('concrete-tr')][:123]
+        print('videos:', videos, sep='\n')
         for video, text in zip(videos, texts):
             if '单元作业' in text or '已完成' in text:
                 continue
@@ -97,10 +102,11 @@ def main():
             print('a new thread started')
             threads.append(thread)
             print('threads:', threads, sep='\n')
+        # set time out in seconds. the parameter should be greater than length of any video to watch.
         for thread in threads:
             thread.join(60 * 60)
         browser.refresh()
-        time.sleep(10)
+        time.sleep(30)
 
 
 if __name__ == '__main__':
